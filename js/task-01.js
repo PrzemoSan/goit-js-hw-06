25542ee6c23f1aa2fddbee551ec49d1530c57bc2
PrzemoSan <bielas102@gmail.com>
2022-12-06 13:01:59 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const ListCategories = document.querySelectorAll('#categories > li');
+
+console.log(`Number of categories: ${ListCategories.length} `)
+
+const categoryElementsCount = ListCategories.forEach((e)=>
+
+{console.log(`Category: ${e.querySelector('h2').textContent}\n
+
+Elements: ${e.querySelector('ul').childElementCount}`)})
