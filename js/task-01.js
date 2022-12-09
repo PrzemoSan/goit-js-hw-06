fdd85b3b99a378661629a3689ae0c81f8d13da86
PrzemoSan <bielas102@gmail.com>
2022-12-09 13:58:55 +0100

    Zad 1 i 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,7 @@
 const ListCategories = document.querySelectorAll('#categories > li');
-
 console.log(`Number of categories: ${ListCategories.length} `)
 
 const categoryElementsCount = ListCategories.forEach((e)=>
-
 {console.log(`Category: ${e.querySelector('h2').textContent}\n
 
-Elements: ${e.querySelector('ul').childElementCount}`)})
+Elements: ${e.querySelector('ul').childElementCount}`)})  
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const findUl = document.querySelector("#ingredients")
+
+const secondTask = ingredients.forEach((ingredient) => {
+  let ingredientList = document.createElement("li");
+  ingredientList.textContent = ingredient;
+  ingredientList.classList.add("item");
+  findUl.append(ingredientList);
+})
